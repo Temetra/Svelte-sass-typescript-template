c00a5165e9c916a64bdc6aa7c265cfff68fc3737
Temetra <temetra.xy+git@gmail.com>
2020-08-11 06:54:59 +0100

    Changed casting in test worker

--- src/modules/test.worker.ts
+++ src/modules/test.worker.ts
@@ -1,10 +1,14 @@
-self.addEventListener("message", evt => {
+// This will be an embedded worker when processed by the loader
+// Cast self from dom to webworker 
+const worker = self as unknown as Worker;
+
+worker.addEventListener("message", evt => {
 	// Log request
 	console.log(`Worker got: ${evt.data}`);
 
-	// Send response in 2000 ms
-	setTimeout(() => (self as any).postMessage("Hello from worker"), 1500);
+	// Send response in x ms
+	setTimeout(() => worker.postMessage("Hello from worker"), 1500);
 });
 
-// This is a dummy export
+// Dummy export to ensure this file is a module
 export default function(){};
