8a7729c832a468f67e984fac25f308e5ee9959f1
Temetra <temetra.xy+git@gmail.com>
2020-08-11 06:18:07 +0100

    Added some types to fetching

--- src/modules/fetching.ts
+++ src/modules/fetching.ts
@@ -1,8 +1,8 @@
-export function waitFor(ms: number, param?: any) {
+export function waitFor<TParam>(ms: number, param?: TParam) : Promise<TParam> {
 	return new Promise(resolve => setTimeout(() => resolve(param), ms));
 }
 
-export function checkResponse(response: any) {
+export function checkResponse(response: Response) {
 	if (!response.ok) throw { message:response.statusText, status:response.status };
 	else return response;
 }
