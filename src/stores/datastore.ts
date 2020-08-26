95b78874e1e511f138aed6a15af065409cddc788
Temetra <temetra.xy+git@gmail.com>
2020-08-26 03:31:55 +0100

    Typescript watch bug is fixed

--- src/stores/datastore.ts
+++ src/stores/datastore.ts
@@ -1,6 +1,3 @@
-// NOTE: Probably a bug - when using livereload, changes to imported 
-// .ts files only propagate to importing modules when saved twice
-
 import { writable } from "svelte/store";
 
 function squareNumber(input: number):number{
